7149fee9e7a6c1d19a0fed4f4c81fb5ee7219907
Adam Wong <adamwthedev@gmail.com>
2020-09-24 09:05:57 -0700

    created Matches Service

---
+++ src/matches/matchesService.js
@@ -0,0 +1,23 @@
+const matchesService = {
+    getAllMatches(knex) {
+        return knex.select('*').from(matches)
+    },
+
+    insertMatch(knex, newMatch) {
+        return knex
+            .insert(newMatch)
+            .into('matches')
+            .returning('*')
+            .then(rows => {
+                return rows[0]
+        })
+    },
+
+    deleteMatch(knex, id) {
+        return knex('matches')
+        .where('id', id)
+        .delete()
+    }
+}
+
+module.exports = matchesService
