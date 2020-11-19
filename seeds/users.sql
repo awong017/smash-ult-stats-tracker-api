f0ee607c069311dc6f8945f72cc3f3c2dd68dc5c
Adam Wong <adamwthedev@gmail.com>
2020-11-18 17:42:34 -0800

    altered id in character migration

---
+++ seeds/users.sql
@@ -0,0 +1,8 @@
+INSERT INTO users(email, username, password)
+	VALUES
+		('awong017@ucr.edu', 'awong017', 'awong017'),
+		('brianphn@gmail.com', 'phanman', 'phantastic'),
+		('markt@gmail.com', 'markt', 'masterdev'),
+		('shaan@gmail.com', 'raisin', 'raisin'), 
+		('alvin@gmail.com', 'alvinv', 'masterdev2')
+    ;
